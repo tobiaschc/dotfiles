e9cba99c79d41a6d3c87c8b44033152f4ec6a7a8
Tobias Chavarria <tobias.chavarria.castro@gmail.com>
2025-11-08 22:29:47 +0100

    feat(nvim): add copilot floating term

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    open_mapping = [[<leader>t]],
+    open_mapping = [[<leader>.]],
     start_in_insert = true,
     persist_mode = false,
     direction = 'float',
@@ -18,11 +18,37 @@ return {
       vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
       vim.keymap.set('n', 'q', ':q<CR>', opts)
       vim.keymap.set('n', '<esc>', ':q<CR>', opts)
-      -- avoid conflict with toggleterm open mapping
-      vim.keymap.set('t', '<leader>t', ' t', opts)
     end,
   },
   config = function(_, opts)
     require('toggleterm').setup(opts)
+
+    -- Custom terminal for GitHub Copilot
+    local Terminal = require('toggleterm.terminal').Terminal
+
+    local copilot = Terminal:new {
+      cmd = 'copilot',
+      dir = 'git_dir',
+      start_in_insert = true,
+      direction = 'float',
+      float_opts = {
+        border = 'rounded',
+        width = math.floor(vim.o.columns * 0.8),
+        height = math.floor(vim.o.lines * 0.8),
+      },
+      on_open = function(term)
+        local optscp = { buffer = term.bufnr, noremap = true, silent = true }
+        -- Exit terminal mode and close window
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], optscp)
+        vim.keymap.set('n', 'q', ':q<CR>', optscp)
+        vim.keymap.set('n', '<esc>', ':q<CR>', optscp)
+      end,
+    }
+
+    function _copilot_toggle()
+      copilot:toggle()
+    end
+
+    vim.api.nvim_set_keymap('n', '<leader>,', '<cmd>lua _copilot_toggle()<CR>', { noremap = true, silent = true, desc = 'Toggle GitHub Copilot Terminal' })
   end,
 }
