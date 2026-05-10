e9e1b1cfe1d7b053f4ca2c9fc2a363561dfd92c3
Tobias Chavarria <tobias.chavarria.castro@gmail.com>
2026-05-10 14:19:51 +0200

    feat(nvim): add codex terminal

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -40,5 +40,13 @@ return {
     end
 
     vim.keymap.set('n', '<leader>,', copilot_toggle, { noremap = true, silent = true, desc = '[T]oggle GitHub Copilot Terminal' })
+
+    -- Codex floating terminal
+    local codex_term = require('toggleterm.terminal').Terminal:new { cmd = 'codex', hidden = true, direction = 'float' }
+    local function codex_toggle()
+      codex_term:toggle()
+    end
+
+    vim.keymap.set('n', '<leader>;', codex_toggle, { noremap = true, silent = true, desc = '[T]oggle Codex Terminal' })
   end,
 }
